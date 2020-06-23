b00548e3b8d3105ac407e9ae57d28755865e3944
Luciano <regueraluciano@gmail.com>
2020-06-23 13:08:43 -0300

    config vars env Mongo

--- server/config/config.js
+++ server/config/config.js
@@ -7,7 +7,7 @@ let urlDB ;
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-    urlDB = 'mongodb+srv://luchithor:6u0Qcsghhjt6nSwW@cluster0-p9vnf.mongodb.net/cafe';
+    urlDB = process.env.MONGO_URI;
 }
 
 process.env.URLDB = urlDB;
